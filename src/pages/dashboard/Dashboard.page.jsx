85d5b073f5dba06663b97060445664044d8814d4
Nabarajrai <nabaraj2055@gmail.com>
2025-08-26 22:01:27 +0545

    cards listed

--- src/pages/dashboard/Dashboard.page.jsx
+++ src/pages/dashboard/Dashboard.page.jsx
@@ -1,5 +1,7 @@
 import HeaderComponent from "../../components/header/Header.component";
 import CardComponent from "../../components/card/Card.component";
+//icons
+import { KeyIcon, CategoryIcon, SecureIcon } from "../../helpers/Icon.helper";
 const DashboardPage = () => {
   return (
     <div className="dashboard-container">
@@ -9,7 +11,30 @@ const DashboardPage = () => {
       <div className="dashboard-main-section">
         <div className="wrapper">
           <div className="dashboard-display-card">
-            <CardComponent />
+            <div className="card-list-section">
+              <CardComponent
+                title="Total Passwords"
+                number="0"
+                icon={<KeyIcon />}
+                iconColor="purple"
+              />
+            </div>
+            <div className="card-list-section">
+              <CardComponent
+                title="Categories"
+                number="7"
+                icon={<CategoryIcon />}
+                iconColor="green"
+              />
+            </div>
+            <div className="card-list-section">
+              <CardComponent
+                title="Security Score"
+                number="Good"
+                icon={<SecureIcon />}
+                iconColor="green"
+              />
+            </div>
           </div>
         </div>
       </div>
