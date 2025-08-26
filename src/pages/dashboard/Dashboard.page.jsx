925888c37e5e2aaec8d1fa9ea4296dd9f50c961e
jack8296 <jackblok904@gmail.com>
2025-08-26 11:18:44 +0000

    card created

--- src/pages/dashboard/Dashboard.page.jsx
+++ src/pages/dashboard/Dashboard.page.jsx
@@ -1,9 +1,17 @@
 import HeaderComponent from "../../components/header/Header.component";
+import CardComponent from "../../components/card/Card.component";
 const DashboardPage = () => {
   return (
     <div className="dashboard-container">
       <div className="header-section">
         <HeaderComponent />
+      </div>
+      <div className="dashboard-main-section">
+        <div className="wrapper">
+          <div className="dashboard-display-card">
+            <CardComponent />
+          </div>
+        </div>
       </div>
     </div>
   );
